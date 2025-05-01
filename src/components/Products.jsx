49ae047758b573af90cd4d174719982ebbbb2c20
naitik <naitikraiyani@gmail.com>
2025-05-01 12:09:46 +0530

    news api

---
+++ src/components/Products.jsx
@@ -0,0 +1,113 @@
+import img1 from "./image/m3.jpg"
+import img2 from "./image/m4.jpg"
+import img3 from "./image/m5.jpg"
+import img4 from "./image/c1.jpg"
+import img5 from "./image/c2.jpg"
+import img6 from "./image/c3.jpg"
+import img7 from "./image/b1.jpg"
+import img8 from "./image/b2.jpg"
+import img9 from "./image/b3.jpg"
+import img10 from "./image/s1.jpg"
+import img11 from "./image/s2.jpg"
+import img12 from "./image/s3.jpg"
+
+export const product = [
+  {
+    img: img1,
+    id: 1,
+    name: "Wireless Mouse",
+    price: 29.99,
+    category: "Electronics",
+    description: "Compact wireless mouse with ergonomic design",
+  },
+  {
+    img: img2,
+    id: 2,
+    name: "Wireless Mouse",
+    price: 49.99,
+    category: "Electronics",
+    description: "Compact wireless mouse with ergonomic design",
+  },
+  {
+    img: img3,
+    id: 3,
+    name: "Wireless Mouse",
+    price: 69.99,
+    category: "Electronics",
+    description: "Compact wireless mouse with ergonomic design",
+  },
+  {
+    img: img4,
+    id: 4,
+    name: "Camera",
+    price: 1099.99,
+    category: "Electronics Device",
+    description: "Compact camera with ergonomic picture",
+  },
+  {
+    img: img5,
+    id: 5,
+    name: "Camera",
+    price: 1599.99,
+    category: "Electronics Device",
+    description: "Compact camera with ergonomic picture",
+  },
+  {
+    img: img6,
+    id: 6,
+    name: "Camera",
+    price: 2099.99,
+    category: "Electronics Device",
+    description: "Compact camera with ergonomic picture",
+  },
+  {
+    img: img7,
+    id: 7,
+    name: "Eat Beautiful Book",
+    price: 189.99,
+    category: "Eat Beautiful",
+    description: "Compact knowladge with ergonomic design",
+  },
+  {
+    img: img8,
+    id: 8,
+    name: "Passion Book",
+    price: 279.99,
+    category: "Passion",
+    description: "Compact knowladge with ergonomic design",
+  },
+  {
+    img: img9,
+    id: 9,
+    name: "All Book",
+    price: 369.99,
+    category: "Book",
+    description: "Compact knowladge with ergonomic design",
+  },
+  {
+    img: img10,
+    id: 10,
+    name: "Men's Jacket",
+    price: 1199.99,
+    category: "Lather",
+    description: "Compact Wear The Unique Design",
+  },
+  {
+    img: img11,
+    id: 11,
+    name: "Men's Blazer",
+    price: 599.99,
+    category: "Cotton",
+    description: "Compact Wear The Unique Design",
+  },
+  {
+    img: img12,
+    id: 12,
+    name: "Men's Shirt",
+    price: 299.99,
+    category: "Cotton",
+    description: "Compact Wear The Unique Design",
+  },
+];
+
+export default product;
