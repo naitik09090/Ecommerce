0b5f988629c6d358a18ccf86879c65cb97093826
naitik <naitikraiyani@gmail.com>
2025-05-01 12:09:46 +0530

    news api

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { product } from '../components/Products.jsx'
+
+
+const SearchBar = () => {
+
+  const [store, setStore] = useState(product)
+
+  const [data, setData] = useState('')
+
+  const itemPerPage = 6;
+
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const LastItem = currentPage * itemPerPage;
+  const FirstItem = LastItem - itemPerPage;
+  const currentItems = store.filter((curValue) => {
+    return curValue.name.toLowerCase().includes(data)
+  }).slice(FirstItem, LastItem)
+
+  const totalPages = Math.ceil(product.length / itemPerPage);
+
+  const pageNumbers = [...Array(totalPages).keys()].map(num => num + 1);
+
+  const getData = (e) => {
+    console.log(e.target.value);
+    setData(e.target.value)
+  }
+
+  return (
+    <div className='container'>
+      <h1 className='h1'>ABC Product</h1>
+      <input type="text" className='input' placeholder='Search Here..' onChange={getData} />
+
+      {
+        currentItems.map((cur) => {
+          return (
+            <>
+              <h1 className='ProductList'>
+                <img src={cur.img} />
+                <p className='type'>{cur.id}</p>
+                <p className='type'>{cur.name}</p>
+                <p className='type'>{cur.price}</p>
+                <p className='type'>{cur.category}</p>
+                <p className='type'>{cur.description}</p>
+              </h1>
+            </>
+          )
+        })
+      }
+
+      <div>
+        {
+          pageNumbers.map(number => (
+            <button className='btn'
+              key={number}
+              onClick={() => setCurrentPage(number)}
+            >
+
+              {number}
+            </button>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
